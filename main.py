2134b6b2688b398f5c41677f18d1dd41c11346ed
K Yogeshwara Reddy <83354232+Yogeshwara1@users.noreply.github.com>
2022-04-03 19:20:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+import os
+from dotenv import load_dotenv
+from neuralintents import GenericAssistant
+
+chatbot = GenericAssistant()
+
+
+client = discord.Client()
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$aibot'):
+        pass
+
+
+client.run(TOKEN)
